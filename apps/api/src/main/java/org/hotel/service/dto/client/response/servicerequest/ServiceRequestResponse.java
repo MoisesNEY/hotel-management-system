cbbf77cbaf116aa79c1732c550c279cc951b7775
yaderpr <yaderpineda011@gmail.com>
2025-12-12 21:38:34 -0600

    feat(api-dtos): agrega response dtos para el portal de clientes

---
+++ apps/api/src/main/java/org/hotel/service/dto/client/response/servicerequest/ServiceRequestResponse.java
@@ -0,0 +1,24 @@
+package org.hotel.service.dto.client.response.servicerequest;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hotel.domain.enumeration.RequestStatus;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.Instant;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class ServiceRequestResponse implements Serializable {
+    private Long id;
+    private Instant requestDate;
+    private String details;
+    private RequestStatus status;
+    private Long serviceId;
+    private String serviceName;
+    private BigDecimal servicePrice;
+    private Long bookingId;
+}
