c63f3eb90651735a38d73d1f4662170629cb0be8
MoisesNEY <moisesney2@gmail.com>
2025-12-12 20:27:35 -0600

    feat: Asocia la web al login personalizado de keycloak

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -2,8 +2,21 @@
 import LandingPage from './pages/LandingPage';
 import './App.css';
 import './styles/landing.css';
+import keycloak from './services/keycloak';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [authenticated, setAuthenticated] = useState(false);
+
+  useEffect(() => {
+    keycloak.init({
+      onLoad: 'check-sso',  // Verifica si hay sesión sin forzar login
+      checkLoginIframe: false
+    }).then(authenticated => {
+      setAuthenticated(authenticated);
+    });
+  }, []);
+
   return (
     <div className="App">
       <LandingPage />
