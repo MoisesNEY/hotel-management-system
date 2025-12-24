d20cf407e6b521b0e2aba90e271385474463f025
Oswaldo Javier Bustamante Martinez <omartinez@eduvia.ai>
2025-12-24 08:32:10 -0600

    Elimina carpeta de mapas que ya no se utiliza en el proyecto

--- apps/web/src/admin/routes.tsx
+++ apps/web/src/admin/routes.tsx
@@ -5,7 +5,6 @@ import {
     CalendarDaysIcon,
     SparklesIcon,
     UsersIcon,
-    MapPinIcon,
     UserIcon,
     ComputerDesktopIcon,
     UserGroupIcon,
@@ -23,7 +22,6 @@ import BookingsView from './views/Bookings/BookingsView';
 import WalkInWizard from './views/WalkIn/WalkInWizard'; // Import WalkInWizard
 import ServicesView from './views/Services/ServicesView';
 import CustomersView from './views/Customers/CustomersView';
-import MapsView from './views/Maps/MapsView';
 import UserProfileView from './views/UserProfile/UserProfileView';
 import TablesView from './views/Tables/TablesView';
 import CMSList from './views/CMS/CMSList';
@@ -117,14 +115,6 @@ const routes: RouteConfig[] = [
         name: 'Clientes',
         icon: UsersIcon,
         component: CustomersView,
-        layout: '/admin',
-        allowedRoles: ['ROLE_ADMIN', 'ROLE_EMPLOYEE']
-    },
-    {
-        path: '/maps',
-        name: 'Mapas',
-        icon: MapPinIcon,
-        component: MapsView,
         layout: '/admin',
         allowedRoles: ['ROLE_ADMIN', 'ROLE_EMPLOYEE']
     },
